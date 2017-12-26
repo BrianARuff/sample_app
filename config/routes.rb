866389352bc971309b60d2c9df34d78cbc38e225
BrianARuff <thelonewolf198927@gmail.com>
2017-12-26 14:55:51 -0500

    pre users signup functionality

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  get 'signup', to: 'users#new'
+
+  get 'home', to: 'static_pages#home'
+  get 'about', to: 'static_pages#about'
+  get 'help', to: 'static_pages#help'
+  get 'helf', to: 'static_pages#help'
+  get 'contact', to: 'static_pages#contact'
+
+  root 'static_pages#home'
+end
