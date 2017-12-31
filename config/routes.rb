98aa7c51f0b6728979928c530c2ab39ea32b3395
Brian Ruff <thelonewolf198927@gmail.com>
2017-12-31 10:34:39 -0500

    flash messages added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+
+  resources :users
 
   get 'home', to: 'static_pages#home'
   get 'about', to: 'static_pages#about'
