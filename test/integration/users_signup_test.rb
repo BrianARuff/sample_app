7489965044f0bafb58bf3bf486bcf364658fedb7
Brian Ruff <thelonewolf198927@gmail.com>
2017-12-31 22:56:55 -0500

    added test for flash, test for valid submission of user object

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,4 +16,22 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'shared/_error_messages'
     assert_template 'users/new'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference('User.count', 1) do
+      post users_path, params: {
+          user: {
+              name:'example user',
+              email:'user@example.com',
+              password: '123456',
+              password_confirmation:'123456'
+          }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
 end
+
